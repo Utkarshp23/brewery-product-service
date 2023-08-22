35fa3fcabe9d065b01edbba41dc3e34d37e0aa1e
utkarsh <utkarshp@cdac.in>
2023-08-22 16:00:49 +0530

    initial commit

---
+++ brewery-product-service/src/main/java/dev/backend/breweryproductservice/BreweryProductServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.backend.breweryproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryProductServiceApplication.class, args);
+	}
+
+}
---
+++ brewery-product-service/src/test/java/dev/backend/breweryproductservice/BreweryProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.backend.breweryproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
